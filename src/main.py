c5cae55a561e1a02d12a9988d49d64499ce4d470
egene-chung <egenechung@gmail.com>
2024-11-22 04:44:43 +0900

    Add: csv 파일 경로 수정

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from data.preprocess import check_and_preprocess_data
 
 if __name__ == "__main__":
     # 파일 경로
-    ow_path = '/data/openworld.csv'
-    cw_path = '/data/closedworld.csv'
+    ow_path = 'data/csv/openworld_data.csv'
+    cw_path = 'data/csv/closedworld_data.csv'
 
     check_and_preprocess_data(ow_path, cw_path)
