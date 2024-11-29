7a523f3f965ccefc773a8a396d18bcfa2ef85339
egene-chung <egenechung@gmail.com>
2024-11-29 22:45:50 +0900

    Modify: closed world random forest 로직 수정

--- src/main.py
+++ src/main.py
@@ -1,23 +1,21 @@
 import pandas as pd
-from itertools import product
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import accuracy_score, classification_report
 from data.preprocess import check_and_preprocess_data
-from sklearn.model_selection import cross_val_score
 from evaluation import evaluate_multi
 
-def train_random_forest(X, y, X_train, X_test, y_train, y_test):
-    
+def train_rf_cw(X, y, X_train, X_test, y_train, y_test):
+    # best performing model in the closed world scenario
+    print("\n========Closed World(Random Forest)========")
     class_names = list(y.unique())
     
-    # 데이터 스케일링
+    # Scale data
     scaler = StandardScaler()
-    X_train_scaled = scaler.fit_transform(X_train)
-    X_test_scaled = scaler.transform(X_test)
-    # X_test_scaled not used because
+    X_train_scaled = pd.DataFrame(scaler.fit_transform(X_train), columns=X.columns)
+    X_test_scaled = pd.DataFrame(scaler.transform(X_test), columns=X.columns)
 
+    # random forest model
     rf_model = RandomForestClassifier(
         n_estimators=50,
         max_depth=None,
@@ -29,43 +27,37 @@ def train_random_forest(X, y, X_train, X_test, y_train, y_test):
         random_state=42
     )
 
-    threshold = 0.01
-
+    # ===Before Feature selection===
     rf_model.fit(X_train_scaled, y_train)
     y_pred = rf_model.predict(X_test_scaled)
-    y_proba = rf_model.predict_proba(X_test_scaled)  # Predicted probabilities
+    y_proba = rf_model.predict_proba(X_test_scaled)
     
-    evaluate_multi(y_test, y_pred, y_proba, class_names, save_path_details="Rf")
+    print("[Before Feature Selection]")
+    evaluate_multi(y_test, y_pred, y_proba, class_names)
 
-    # Feature Importance
-    feature_importances = pd.Series(rf_model.feature_importances_, index=X.columns).sort_values(ascending=False)
-    
-    # print(feature_importances.to_string() + "\n")
-    # selected_features = feature_importances[feature_importances > threshold].index
-    selected_features = feature_importances.iloc[:-9].index
-
+    # ===After Feature Selection===
+    print("[After Feature Selection]")
+    print("<Best Performance - through Forward Elimination in Random Forest>")
+    # Feature Selection by applying Foward Elimination based on Feature Importance
+    # Experiment in finding these features can be found in /src/experiments/closedworld/cw_rf_forward_elimination.txt
+    selected_features = ['num_incoming_cum', 'std_total', 'num_incoming_thirty', 'std_dev_interval', 'std_outgoing_burst', 'std_total_cum', 'num_outgoing_last_thirty', 'avg_cum_incoming', 'std_timestamps', 'num_incoming_burst', 'num_outgoing_cum']
 
     X_train_selected = X_train[selected_features]
     X_test_selected = X_test[selected_features]
-
-    # 모델 학습
-    rf_model.fit(X_train_selected, y_train)
-    y_pred = rf_model.predict(X_test_selected)
     
-    # 성능 평가
-    # accuracy = accuracy_score(y_test, y_pred)
-    # cross val
-    # scores = cross_val_score(rf_model, X_train[selected_features], y_train, cv=5, scoring="accuracy")
-    # print(f"Cross-Validation Accuracy: {scores.mean():.4f} ± {scores.std():.4f}")
+    X_train_selected_scaled = pd.DataFrame(scaler.fit_transform(X_train_selected), columns=selected_features)
+    X_test_selected_scaled = pd.DataFrame(scaler.transform(X_test_selected), columns=selected_features)
 
-    class_names = list(y.unique())  # Replace with your actual class names
-    y_proba = rf_model.predict_proba(X_test_selected)  # Predicted probabilities
-    print("===Best Performance in Closed World Scenario===")
-    print("After Backward Elimination in Random Forest")
-    evaluate_multi(y_test, y_pred, y_proba, class_names, save_path_details="Rf_ft_selection")
+    # train model again after feature selection
+    rf_model.fit(X_train_selected_scaled, y_train)
+    y_pred = rf_model.predict(X_test_selected_scaled)
+    y_proba = rf_model.predict_proba(X_test_selected_scaled)
+    evaluate_multi(y_test, y_pred, y_proba, class_names, save_path_details="cw_rf", save_plots=True)
 
 
-# def train_
+    
+
+
 
 def main():
     # 파일 경로
@@ -83,7 +75,7 @@ def main():
     # 데이터셋 분리
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y, random_state=42)
 
-    train_random_forest(X, y, X_train, X_test, y_train, y_test)
+    train_rf_cw(X, y, X_train, X_test, y_train, y_test)
     
 
 if __name__ == "__main__":
