27abbb1b523617d167e04c0aae081a0151a9a351
egene-chung <egenechung@gmail.com>
2024-12-02 03:42:38 +0900

    Modify: evaluate method plotting 추가

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,10 +1,13 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.metrics import (
     accuracy_score, 
     precision_score, 
     recall_score, 
     f1_score, 
     roc_auc_score,
+    precision_recall_curve,
+    roc_curve
 )
 
 
@@ -47,3 +50,27 @@ def evaluate_binary(y_true, y_pred, y_prob=None):
     if y_prob is not None:
         auc_roc = roc_auc_score(y_true, y_prob[:, 1])
         print(f"AUC-ROC: {auc_roc:.4f}")
+
+        # Precision-Recall Curve
+        precisions, recalls, _ = precision_recall_curve(y_true, y_prob[:, 1])
+        plt.figure()
+        plt.plot(recalls, precisions, label="Precision-Recall Curve")
+        plt.xlabel("Recall")
+        plt.ylabel("Precision")
+        plt.title("Precision-Recall Curve")
+        plt.legend(loc="best")
+        plt.grid()
+        plt.savefig(f"results/ow_binary_precision_recall_curve.png")
+        plt.close()
+
+        # ROC Curve
+        fpr, tpr, _ = roc_curve(y_true, y_prob[:, 1])
+        plt.figure()
+        plt.plot(fpr, tpr, label=f"ROC Curve (AUC = {auc_roc:.4f})")
+        plt.xlabel("False Positive Rate")
+        plt.ylabel("True Positive Rate")
+        plt.title("ROC Curve")
+        plt.legend(loc="best")
+        plt.grid()
+        plt.savefig(f"results/ow_binary_roc_curve.png")
+        plt.close()
