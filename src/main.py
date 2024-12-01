0c11e46ed466423abcf63bc7840a20cbe8cea036
egene-chung <egenechung@gmail.com>
2024-12-02 03:39:20 +0900

    Modify: main.py best performance로 업데이트

--- src/main.py
+++ src/main.py
@@ -1,195 +1,95 @@
 import pandas as pd
-from sklearn import svm
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.preprocessing import StandardScaler
+
 from data.preprocess import check_and_preprocess_data
-from sklearn.metrics import accuracy_score
-from sklearn.feature_selection import SelectKBest, f_classif, VarianceThreshold
-from imblearn.over_sampling import SMOTE
+from evaluate import evaluate_multi, evaluate_binary
 
-from evaluation import evaluate_multi, evaluate_binary
-
-def train_rf_cw(X, y, X_train, X_test, y_train, y_test):
-    # best performing model in the closed world scenario
-    print("\n========Closed World(Random Forest)========")
-    class_names = list(y.unique())
+def train_randomforest(
+    file_path, 
+    scenario, 
+    n_estimators=100, 
+    max_depth=None, 
+    min_samples_split=2, 
+    min_samples_leaf=1, 
+    max_features='sqrt', 
+    selected_features=None
+):
+    # load dataset
+    df = pd.read_csv(file_path)
     
-    # Scale data
+    X = df.drop(columns=['Label'])
+    y = df['Label']
+    
+    # data scaling
     scaler = StandardScaler()
-    X_train_scaled = pd.DataFrame(scaler.fit_transform(X_train), columns=X.columns)
-    X_test_scaled = pd.DataFrame(scaler.transform(X_test), columns=X.columns)
+    X_scaled = pd.DataFrame(scaler.fit_transform(X), columns=X.columns)
 
+    X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, stratify=y, random_state=42)
+    
     # random forest model
-    # these parameters were selected through iterative testing across various hyperparameter combinations.
-    # can be found in /src/experiments/closedworld/cw_rf_backward_elimination.txt
-    rf_model = RandomForestClassifier(
-        n_estimators=50,
-        max_depth=None,
-        min_samples_split=2,
-        min_samples_leaf=1,
-        max_features="log2",
-        bootstrap=False,
-        criterion="gini",
+    model = RandomForestClassifier(
+        n_estimators=n_estimators,
+        max_depth=max_depth,
+        min_samples_split=min_samples_split,
+        min_samples_leaf=min_samples_leaf,
+        max_features=max_features,
         random_state=42
     )
-
-    # ===Before Feature selection===
-    rf_model.fit(X_train_scaled, y_train)
-    y_pred = rf_model.predict(X_test_scaled)
-    y_proba = rf_model.predict_proba(X_test_scaled)
     
-    print("[Before Feature Selection]")
-    evaluate_multi(y_test, y_pred, y_proba, class_names)
+    # train model before feature selection
+    print(f"\n====== {scenario}======")
+    if selected_features:
+        print("[Before Feature Selection]")
 
-    # ===After Feature Selection===
-    print("[After Feature Selection]")
-    print("<Best Performance - through Forward Elimination in Random Forest>")
-    # Feature Selection by applying Foward Elimination based on Feature Importance
-    # Experiment in finding these features can be found in /src/experiments/closedworld/cw_rf_forward_elimination.txt
-    selected_features = ['num_incoming_cum', 'std_total', 'num_incoming_thirty', 'std_dev_interval', 'std_outgoing_burst', 'std_total_cum', 'num_outgoing_last_thirty', 'avg_cum_incoming', 'std_timestamps', 'num_incoming_burst', 'num_outgoing_cum']
-
-    X_train_selected = X_train[selected_features]
-    X_test_selected = X_test[selected_features]
+    model.fit(X_train, y_train)
+    y_pred_all = model.predict(X_test)
+    y_proba_all = model.predict_proba(X_test)
     
-    X_train_selected_scaled = pd.DataFrame(scaler.fit_transform(X_train_selected), columns=selected_features)
-    X_test_selected_scaled = pd.DataFrame(scaler.transform(X_test_selected), columns=selected_features)
+    # ==evaluation==
+    if scenario == "Open World Binary" : 
+        evaluate_binary(y_test,y_pred_all, y_proba_all)
+    else : 
+        evaluate_multi(y_test,y_pred_all)
 
-    # train model again after feature selection
-    rf_model.fit(X_train_selected_scaled, y_train)
-    y_pred = rf_model.predict(X_test_selected_scaled)
-    y_proba = rf_model.predict_proba(X_test_selected_scaled)
-    evaluate_multi(y_test, y_pred, y_proba, class_names, save_path_details="cw_rf", save_plots=True)
-
-def train_rf_cw(X, y, X_train, X_test, y_train, y_test):
-    # best performing model in the closed world scenario
-    print("\n========Open World-multi(Random Forest)========")
-    class_names = list(y.unique())
     
-    # Scale data
-    scaler = StandardScaler()
-    X_train_scaled = pd.DataFrame(scaler.fit_transform(X_train), columns=X.columns)
-    X_test_scaled = pd.DataFrame(scaler.transform(X_test), columns=X.columns)
-
-    # random forest model
-    # these parameters were selected through iterative testing across various hyperparameter combinations.
-    # can be found in /src/experiments/closedworld/cw_rf_backward_elimination.txt
-    rf_model = RandomForestClassifier(
-        n_estimators=50,
-        max_depth=None,
-        min_samples_split=2,
-        min_samples_leaf=1,
-        max_features="log2",
-        bootstrap=False,
-        criterion="gini",
-        random_state=42
-    )
-
-    # ===Before Feature selection===
-    rf_model.fit(X_train_scaled, y_train)
-    y_pred = rf_model.predict(X_test_scaled)
-    y_proba = rf_model.predict_proba(X_test_scaled)
-    
-    print("[Before Feature Selection]")
-    evaluate_multi(y_test, y_pred, y_proba, class_names)
-
-    # ===After Feature Selection===
-    print("[After Feature Selection]")
-    print("<Best Performance - through Forward Elimination in Random Forest>")
-    # Feature Selection by applying Foward Elimination based on Feature Importance
-    # Experiment in finding these features can be found in /src/experiments/closedworld/cw_rf_forward_elimination.txt
-    selected_features = ['num_incoming_cum', 'std_total', 'num_incoming_thirty', 'std_dev_interval', 'std_outgoing_burst', 'std_total_cum', 'num_outgoing_last_thirty', 'avg_cum_incoming', 'std_timestamps', 'num_incoming_burst', 'num_outgoing_cum']
-
-    X_train_selected = X_train[selected_features]
-    X_test_selected = X_test[selected_features]
-    
-    X_train_selected_scaled = pd.DataFrame(scaler.fit_transform(X_train_selected), columns=selected_features)
-    X_test_selected_scaled = pd.DataFrame(scaler.transform(X_test_selected), columns=selected_features)
-
-    # train model again after feature selection
-    rf_model.fit(X_train_selected_scaled, y_train)
-    y_pred = rf_model.predict(X_test_selected_scaled)
-    y_proba = rf_model.predict_proba(X_test_selected_scaled)
-    evaluate_multi(y_test, y_pred, y_proba, class_names, save_path_details="osw_rf", save_plots=True)
-
-def train_svm_ow_binary(X, y, X_train, X_test, y_train, y_test, num_features=50, C_value=2048, gamma_value=0.25):
-    print("\n========Open World- Binary (SVM)========")
-
-    # 1. 처리되지 않은 데이터
-    print("\n--- Using Raw Data ---")
-    # Scale the features to the range [-1, 1]
-    scaler = MinMaxScaler(feature_range=(-1, 1))
-    X_train_scaled = scaler.fit_transform(X_train)
-    X_test_scaled = scaler.transform(X_test)
-
-    # Train and evaluate SVM
-    svm_model = svm.SVC(kernel='rbf', C=C_value, gamma=gamma_value, probability=True)
-    svm_model.fit(X_train_scaled, y_train)
-    y_pred = svm_model.predict(X_test_scaled)
-    y_proba = svm_model.predict_proba(X_test_scaled)[:, 1] 
-
-    evaluate_binary(y_test, y_pred, y_proba, save_plots=False)    
-
-    # 2. SMOTE + SelectKBest 적용
-    print("\n--- Using SMOTE + SelectKBest ---")
-    # # Apply SMOTE
-    smote = SMOTE(random_state=42)
-    X_train_resampled, y_train_resampled = smote.fit_resample(X_train, y_train)
-
-    # Apply SelectKBest
-    selector = SelectKBest(score_func=f_classif, k=num_features)
-    X_train_selected = selector.fit_transform(X_train_resampled, y_train_resampled)
-    X_test_selected = selector.transform(X_test)
-
-    # Scale the features
-    X_train_selected_scaled = scaler.fit_transform(X_train_selected)
-    X_test_selected_scaled = scaler.transform(X_test_selected)
-
-    # Train and evaluate SVM
-    svm_model.fit(X_train_selected_scaled, y_train_resampled)
-    y_pred_selected = svm_model.predict(X_test_selected_scaled)
-    y_proba_selected = svm_model.predict_proba(X_test_selected_scaled)[:, 1]
-    evaluate_binary(y_test, y_pred_selected, y_proba_selected, save_path_details="smote_selectkbest", save_plots="ow_binary")
-
+    # train with selected features (if provided)
+    if selected_features:
+        print("\n[After Feature Selection]")
+        
+        X_train_selected = X_train[selected_features]
+        X_test_selected = X_test[selected_features]
+        
+        # re-train same model with selected features
+        model.fit(X_train_selected, y_train)
+        y_pred_selected = model.predict(X_test_selected)
+        y_proba = model.predict_proba(X_test_selected)
+        
+        # ==evaluation==
+        if scenario == "Open World Binary" : 
+            evaluate_binary(y_test,y_pred_selected, y_proba)
+        else : 
+            evaluate_multi(y_test,y_pred_selected)
 
 def main():
-    # 파일 경로
+    # file paths
     cw_path = 'data/csv/closedworld_data.csv'
     ow_binary_path = 'data/csv/openworld_binary_data.csv'
     ow_multi_path = 'data/csv/openworld_multi_data.csv'
 
     check_and_preprocess_data(cw_path, ow_binary_path, ow_multi_path)
 
-    # Best performing model and params in closed world
-    df_cw = pd.read_csv(cw_path)
-    X_cw = df_cw.drop(columns=['Label'])
-    y_cw = df_cw['Label']
-
-    X_train, X_test, y_train, y_test = train_test_split(X_cw, y_cw, test_size=0.2, stratify=y_cw, random_state=42)
-
-    train_rf_cw(X_cw, y_cw, X_train, X_test, y_train, y_test)
+    # best performing model + hyperparameters in scenarios
+    # the methods and results of feature selection can be found in /feature_selection
+    cw_feature_selection = ['num_incoming_cum', 'fraction_outgoing', 'num_incoming_thirty', 'max_interval', 'std_outgoing_burst', 'std_total_cum', 'num_outgoing_last_thirty', 'avg_cum_incoming', 'fraction_incoming_burst', 'std_total_burst', 'std_incoming_cum', 'std_timestamps', 'fraction_outgoing_burst_outgoing_diff']
+    ow_binary_feature_selection = ['num_outgoing_last_thirty', 'fraction_outgoing_burst', 'num_total_cum', 'sum_incoming_outgoing_total_cum', 'num_incoming_burst', 'fraction_incoming_burst', 'sum_incoming_outgoing_total_burst_diff', 'sum_incoming_outgoing_total_burst', 'num_total_burst', 'num_outgoing_burst', 'num_total', 'num_incoming_cum', 'sum_incoming_outgoing_total', 'num_incoming', 'num_outgoing', 'std_outgoing_cum', 'avg_cum_incoming', 'fraction_incoming', 'fraction_incoming_burst_incoming_diff', 'avg_total_cum', 'fraction_outgoing', 'fraction_outgoing_burst_outgoing_diff', 'std_incoming_cum', 'std_total_cum', 'avg_total', 'std_total_burst', 'std_total', 'std_incoming_burst', 'mean_interval', 'std_dev_interval', 'range_interval', 'max_interval', 'avg_burst_incoming', 'std_timestamps', 'avg_total_burst', 'avg_timestamps', 'num_outgoing_thirty_cum', 'std_outgoing_burst', 'max_timestamps', 'avg_burst_outgoing', 'num_outgoing_cum', 'num_incoming_thirty', 'num_outgoing_thirty', 'num_incoming_thirty_cum', 'sum_incoming_outgoing_total_cum_diff']
+    ow_multi_feature_selection = ['num_outgoing_thirty_cum', 'num_outgoing_cum', 'std_incoming_burst', 'avg_timestamps', 'fraction_incoming_burst', 'fraction_outgoing_burst', 'num_incoming_burst', 'num_outgoing_burst', 'std_total_burst', 'num_total_burst', 'sum_incoming_outgoing_total_burst', 'num_incoming_last_thirty', 'num_outgoing_last_thirty', 'fraction_outgoing', 'sum_incoming_outgoing_total_burst_diff', 'std_timestamps', 'std_total', 'sum_incoming_outgoing_total_cum', 'num_total_cum', 'fraction_incoming', 'num_total', 'num_incoming', 'num_incoming_cum', 'fraction_incoming_burst_incoming_diff', 'avg_total', 'fraction_outgoing_burst_outgoing_diff', 'sum_incoming_outgoing_total', 'mean_interval', 'avg_burst_incoming', 'avg_total_burst', 'avg_total_cum', 'avg_cum_incoming', 'range_interval', 'num_outgoing', 'max_interval', 'std_total_cum', 'std_dev_interval', 'std_incoming_cum', 'max_timestamps', 'avg_burst_outgoing', 'std_outgoing_burst', 'num_incoming_thirty', 'num_outgoing_thirty', 'sum_incoming_outgoing_total_cum_diff', 'num_incoming_thirty_cum']
     
-    # Open world binary classification
-    df_ow = pd.read_csv(ow_binary_path)
-    X_ow = df_ow.drop(columns=['Label'])
-    y_ow = df_ow['Label']
+    train_randomforest(cw_path, scenario="Closed World", n_estimators=200, max_depth=30, min_samples_split=2, min_samples_leaf=1, max_features="sqrt", selected_features=cw_feature_selection)
+    train_randomforest(ow_binary_path, scenario="Open World Binary", n_estimators=100, max_depth=None, min_samples_split=2, min_samples_leaf=1, max_features="log2", selected_features=ow_binary_feature_selection)
+    train_randomforest(ow_multi_path, scenario="Open World Multi", n_estimators=200, max_depth=30, min_samples_split=2, min_samples_leaf=1, max_features="log2", selected_features=ow_multi_feature_selection)
 
-    var_thresh = VarianceThreshold(threshold=0.0)  # 분산이 0인 feature 제거
-    X_ow_reduced = var_thresh.fit_transform(X_ow)
-
-    X_train, X_test, y_train, y_test = train_test_split(X_ow_reduced, y_ow, test_size=0.2, stratify=y_ow, random_state=42)
-    
-    train_svm_ow_binary(X_ow, y_ow, X_train, X_test, y_train, y_test)
-
-    # Open world Multi classification
-    df_ow_multi = pd.read_csv(ow_multi_path)
-    X_owm = df_ow_multi.drop(columns=['Label'])
-    y_owm = df_ow_multi['Label']
-
-    X_train, X_test, y_train, y_test = train_test_split(X_owm, y_owm, test_size=0.2, stratify=y_owm, random_state=42)
-
-    train_rf_owm(X_owm, y_owm, X_train, X_test, y_train, y_test)
-    
 
 if __name__ == "__main__":
     main()
